aa70c948e0788861e7c80a3d59dacd9036f3621a
SpencerKing7 <spencerking.c@gmail.com>
2024-08-06 23:51:06 -0600

    change router things

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from './components/Navbar'
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import theme from './theme'
@@ -12,17 +12,15 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <BrowserRouter>
-          {/* <NavBar /> */}
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route />
-            <Route />
-            <Route />
-            <Route />
-            <Route />
-          </Routes>
-        </BrowserRouter>
+        {/* <NavBar /> */}
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
       </ThemeProvider>
     </>
   );
